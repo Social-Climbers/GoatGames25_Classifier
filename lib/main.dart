a002bd49ec6a39bcd8b0982daada27765cee1879
Hariras Tongyai <mlhariras.tongyai@gmail.com>
2025-03-19 19:11:59 +0700

    By top 10 routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:goatgames2025classifier/routeData.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,319 +39,77 @@ class ScoreCardScreen extends StatefulWidget {
 
 class _ScoreCardScreenState extends State<ScoreCardScreen> {
   bool isMale = false;
+  bool disableWeight = false;
   String tier = "Unclassified";
   double totalScore = 0;
   double requiredScore = 0;
   TextEditingController MinScoreTextController = TextEditingController();
-  final List<Map<String, dynamic>> routes = [
-    {
-      "Route": "Route 1",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 100,
-      "Top Points": 50,
-      "Zone Weight": 20,
-      "Top Weight": 30,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.green,
-      "Color2": Colors.yellow,
-    },
-    {
-      "Route": "Route 2",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 100,
-      "Top Points": 50,
-      "Zone Weight": 20,
-      "Top Weight": 31,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.green,
-      "Color2": Colors.yellow,
-    },
-    {
-      "Route": "Route 3",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 95,
-      "Top Points": 50,
-      "Zone Weight": 22,
-      "Top Weight": 28,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.green,
-      "Color2": Colors.green,
-    },
-    {
-      "Route": "Route 4",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 95,
-      "Top Points": 50,
-      "Zone Weight": 22,
-      "Top Weight": 28,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.green,
-      "Color2": Colors.green,
-    },
-    {
-      "Route": "Route 5",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 95,
-      "Top Points": 45,
-      "Zone Weight": 22,
-      "Top Weight": 29,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.green,
-      "Color2": Colors.green,
-    },
-    {
-      "Route": "Route 6",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 90,
-      "Top Points": 45,
-      "Zone Weight": 23,
-      "Top Weight": 29,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.green,
-      "Color2": Colors.blue,
-    },
-    {
-      "Route": "Route 7",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 95,
-      "Top Points": 45,
-      "Zone Weight": 22,
-      "Top Weight": 29,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.green,
-      "Color2": Colors.blue,
-    },
-    {
-      "Route": "Route 8",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 90,
-      "Top Points": 45,
-      "Zone Weight": 23,
-      "Top Weight": 29,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.green,
-      "Color2": Colors.blue,
-    },
-    {
-      "Route": "Route 9",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 90,
-      "Top Points": 45,
-      "Zone Weight": 24,
-      "Top Weight": 30,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.green,
-      "Color2": Colors.blue,
-    },
-    {
-      "Route": "Route 10",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 85,
-      "Top Points": 40,
-      "Zone Weight": 24,
-      "Top Weight": 30,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": true,
-      "Color1": Colors.blue,
-      "Color2": Colors.blue,
-    },
-    {
-      "Route": "Route 11",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 80,
-      "Top Points": 40,
-      "Zone Weight": 25,
-      "Top Weight": 31,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.blue,
-      "Color2": Colors.blue,
-    },
-    {
-      "Route": "Route 12",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 80,
-      "Top Points": 40,
-      "Zone Weight": 25,
-      "Top Weight": 31,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.blue,
-      "Color2": Colors.blue,
-    },
-    {
-      "Route": "Route 13",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 80,
-      "Top Points": 40,
-      "Zone Weight": 26,
-      "Top Weight": 32,
-      "Required for Ibex Male": true,
-      "Required for Ibex Female": false,
-      "Color1": Colors.blue,
-      "Color2": Colors.purple,
-    },
-    {
-      "Route": "Route 14",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 75,
-      "Top Points": 35,
-      "Zone Weight": 26,
-      "Top Weight": 32,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.blue,
-      "Color2": Colors.purple,
-    },
-    {
-      "Route": "Route 15",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 75,
-      "Top Points": 35,
-      "Zone Weight": 27,
-      "Top Weight": 33,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.blue,
-      "Color2": Colors.purple,
-    },
-    {
-      "Route": "Route 16",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 70,
-      "Top Points": 35,
-      "Zone Weight": 27,
-      "Top Weight": 33,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.blue,
-      "Color2": Colors.purple,
-    },
-    {
-      "Route": "Route 17",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 70,
-      "Top Points": 35,
-      "Zone Weight": 28,
-      "Top Weight": 34,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.purple,
-      "Color2": Colors.purple,
-    },
-    {
-      "Route": "Route 18",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 65,
-      "Top Points": 30,
-      "Zone Weight": 28,
-      "Top Weight": 34,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.purple,
-      "Color2": Colors.purple,
-    },
-    {
-      "Route": "Route 19",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 65,
-      "Top Points": 30,
-      "Zone Weight": 29,
-      "Top Weight": 35,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.purple,
-      "Color2": Colors.black,
-    },
-    {
-      "Route": "Route 20",
-      "Zone": false,
-      "Top": false,
-      "Zone Points": 60,
-      "Top Points": 30,
-      "Zone Weight": 29,
-      "Top Weight": 35,
-      "Required for Ibex Male": false,
-      "Required for Ibex Female": false,
-      "Color1": Colors.purple,
-      "Color2": Colors.black,
-    },
-  ];
 
   void _calculateScore() {
-    double totalZonePoints = 0;
-    double totalTopPoints = 0;
+    double totalScore = 0;
     bool completedAllSilverhorn = true;
-    bool completedRoute13 = false;
     bool climbedAnyIbex = false;
 
+    // Reset the top 10 flag for all routes
     for (var route in routes) {
+      route["isTop10"] = false;
+    }
+
+    // Filter routes where Top is checked
+    var topRoutes = routes.where((route) => route["Top"] == true).toList();
+
+    // Sort the routes by route number in descending order
+    topRoutes.sort((a, b) {
+      int aRouteNumber = int.parse(a["Route"].split(" ")[1]);
+      int bRouteNumber = int.parse(b["Route"].split(" ")[1]);
+      return bRouteNumber.compareTo(aRouteNumber);
+    });
+
+    // Take the top 10 routes
+    topRoutes = topRoutes.take(10).toList();
+
+    // Mark the top 10 routes
+    for (var route in topRoutes) {
+      route["isTop10"] = true;
+    }
+
+    for (var route in topRoutes) {
       int routeNumber = int.parse(route["Route"].split(" ")[1]);
 
-      if (routeNumber >= 13 && (route["Zone"] || route["Top"])) {
+      if (routeNumber >= 13 && route["Top"]) {
         climbedAnyIbex = true;
       }
 
       bool isSilverhorn =
           (isMale && routeNumber <= 12) || (!isMale && routeNumber <= 9);
 
-      if (route["Zone"]) {
-        totalZonePoints += route["Zone Points"] * route["Zone Weight"];
+      if (route["Top"]) {
+        totalScore +=
+            route["Top Points"] * (disableWeight ? 1 : route["Top Weight"]);
       } else if (isSilverhorn) {
         completedAllSilverhorn = false;
       }
 
-      if (route["Top"]) {
-        totalTopPoints += route["Top Points"] * route["Top Weight"];
-      } else if (isSilverhorn) {}
+      if (route["Zone"]) {
+        totalScore +=
+            route["Zone Points"] * (disableWeight ? 1 : route["Zone Weight"]);
+      }
 
-      // if ((isMale && route["Required for Ibex Male"]) ||
-      //     (!isMale && route["Required for Ibex Female"])) {
-      //   // if (route["Zone"] || route["Top"]) {
-      //   //   completedRoute13 = true;
-      //   // }
-      // }
+      if ((!isMale && routeNumber >= 10 && route["Top"]) ||
+          (isMale && routeNumber >= 13 && route["Top"])) {
+        climbedAnyIbex = true;
+      }
     }
 
-    totalScore = totalZonePoints + totalTopPoints;
-
-    //double requiredScore = isMale == false ? 40800 : 34000;
-    tier = (totalScore >= requiredScore) ? "Ibex" : "Silverhorn";
-
+    this.totalScore = totalScore;
+    tier =
+        (totalScore >= requiredScore || climbedAnyIbex) ? "Ibex" : "Silverhorn";
+    print(this.totalScore);
     setState(() {});
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
     requiredScore = isMale == false ? 40800 : 34000;
@@ -403,10 +162,18 @@ class _ScoreCardScreenState extends State<ScoreCardScreen> {
                                 contentPadding: EdgeInsets.symmetric(
                                     vertical: 5, horizontal: 10),
                               ),
+                              onSubmitted: (value) {
+                                setState(() {
+                                  requiredScore =
+                                      double.tryParse(value) ?? requiredScore;
+                                  _calculateScore();
+                                });
+                              },
                               onChanged: (value) {
                                 setState(() {
                                   requiredScore =
                                       double.tryParse(value) ?? requiredScore;
+                                  _calculateScore();
                                 });
                               },
                             ),
@@ -475,11 +242,6 @@ class _ScoreCardScreenState extends State<ScoreCardScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    // ElevatedButton(
-                    //   onPressed: _calculateScore,
-                    //   child: Text("Calculate"),
-                    // ),
-                    // SizedBox(width: 10),
                     ElevatedButton(
                       onPressed: () {
                         setState(() {
@@ -493,6 +255,17 @@ class _ScoreCardScreenState extends State<ScoreCardScreen> {
                       },
                       child: Text("Clear"),
                     ),
+                    SizedBox(width: 10),
+                    Checkbox(
+                      value: disableWeight,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          disableWeight = value ?? false;
+                          _calculateScore();
+                        });
+                      },
+                    ),
+                    Text("Disable Weight"),
                   ],
                 ),
               ],
@@ -530,6 +303,9 @@ class _ScoreCardScreenState extends State<ScoreCardScreen> {
                   DataColumn(
                       label: Text("Total Pts",
                           style: TextStyle(fontWeight: FontWeight.bold))),
+                  DataColumn(
+                      label: Text("Top 10",
+                          style: TextStyle(fontWeight: FontWeight.bold))),
                 ],
                 rows: routes.map((route) {
                   Color rowColor = Colors.transparent;
@@ -539,11 +315,16 @@ class _ScoreCardScreenState extends State<ScoreCardScreen> {
                   if (route["Top"]) {
                     rowColor = Colors.grey[300]!;
                   }
+                  if (route["isTop10"]) {
+                    rowColor = Colors.blue[100]!;
+                  }
                   double totalPoints = (route["Zone"]
-                          ? route["Zone Points"] * route["Zone Weight"]
+                          ? route["Zone Points"] *
+                              (disableWeight ? 1 : route["Zone Weight"])
                           : 0) +
                       (route["Top"]
-                          ? route["Top Points"] * route["Top Weight"]
+                          ? route["Top Points"] *
+                              (disableWeight ? 1 : route["Top Weight"])
                           : 0);
                   return DataRow(
                     color: MaterialStateProperty.resolveWith<Color?>(
@@ -595,6 +376,9 @@ class _ScoreCardScreenState extends State<ScoreCardScreen> {
                           onChanged: (bool? value) {
                             setState(() {
                               route["Zone"] = value ?? false;
+                              if (!route["Zone"]) {
+                                route["Top"] = false;
+                              }
                               _calculateScore();
                             });
                           },
@@ -641,7 +425,16 @@ class _ScoreCardScreenState extends State<ScoreCardScreen> {
                       ),
                       DataCell(Text(route["Top Weight"].toString())),
                       DataCell(Text(totalPoints.toStringAsFixed(2),
-                          style: TextStyle(fontWeight: FontWeight.bold))),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: !route["isTop10"]
+                                  ? Colors.grey
+                                  : Colors.black))),
+                      DataCell(
+                        route["isTop10"]
+                            ? Icon(Icons.check, color: Colors.green)
+                            : SizedBox.shrink(),
+                      ),
                     ],
                   );
                 }).toList(),
